14c94e6013ca65c596cb9552160e6af886bdb63b
micr0gentoo <26364458+micr0-dev@users.noreply.github.com>
2024-06-30 01:18:06 -0400

    month not months, tarmie is mad

--- static/posts.js
+++ static/posts.js
@@ -43,27 +43,33 @@ function timeSince(date) {
 
     let interval = seconds / 31536000;
 
-    if (interval > 1) {
-        return Math.floor(interval) + " years ago";
+    if (interval >= 1) {
+        const years = Math.floor(interval);
+        return years === 1 ? "1 year ago" : years + " years ago";
     }
     interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + " months ago";
+    if (interval >= 1) {
+        const months = Math.floor(interval);
+        return months === 1 ? "1 month ago" : months + " months ago";
     }
     interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + " days ago";
+    if (interval >= 1) {
+        const days = Math.floor(interval);
+        return days === 1 ? "1 day ago" : days + " days ago";
     }
     interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + " hours ago";
+    if (interval >= 1) {
+        const hours = Math.floor(interval);
+        return hours === 1 ? "1 hour ago" : hours + " hours ago";
     }
     interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + " minutes ago";
+    if (interval >= 1) {
+        const minutes = Math.floor(interval);
+        return minutes === 1 ? "1 minute ago" : minutes + " minutes ago";
     }
 
-    return Math.floor(seconds) + " seconds ago";
+    const sec = Math.floor(seconds);
+    return sec === 1 ? "1 second ago" : sec + " seconds ago";
 }
 
 
